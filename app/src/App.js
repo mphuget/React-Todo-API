61171d7a0c10a7b4709e45cb5fb84803d13b0140
Marc-Philippe Huget <mphuget@gmail.com>
2020-10-14 16:04:32 +0200

    Add and Read from backend API

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
+async function makeGetRequest() {
+
+  let res = await axios.get('http://localhost:3000/api/v1/todos');
+
+  let data = res.data;
+
+  return data;
+}
+
+async function makePostRequest() {
+
+  let res = await axios.post('http://localhost:3000/api/v1/todo', {
+    title: "Mon premier todo",
+    description: "Et sa description"
+  });
+
+  return res;
+
+}
+
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    //addTodo();
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = () => {
+    makeGetRequest()
+    .then(( data ) => setTodos(data))
+    .catch((err) => console.log(err))
+  }
+  
+  const addTodo = () => {
+    makePostRequest()
+    .then(( data ) => console.log(data))
+    .catch((err) => console.log(err))
+  }
+
+  console.log("Todos: ");
+  console.log(todos);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <ul>
+      {
+        todos.map((value, index) => {
+          return <li key={index}> {value.title} </li>
+        } )
+      }
+      </ul>
+
     </div>
   );
 }
